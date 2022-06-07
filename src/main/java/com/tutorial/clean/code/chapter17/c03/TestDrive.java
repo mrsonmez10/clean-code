7ca9cbac58a416631fce835f30df6ad02f256b40
frknsnmz <furkan.sonmez@bilgiedu.net>
2022-06-08 01:09:51 +0300

    Course ended.

--- src/main/java/com/tutorial/clean/code/chapter17/c03/TestDrive.java
+++ src/main/java/com/tutorial/clean/code/chapter17/c03/TestDrive.java
@@ -1,10 +1,20 @@
 package com.tutorial.clean.code.chapter17.c03;
 
+import java.util.Optional;
+import java.util.UUID;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.LinkedList;
+// Or
+import java.util.*;
+
 public class TestDrive {
 
     public static void main(String[] args) {
         /*
-
+            - importlara dikkat edelim. Aynı kütüphaneden 80 satır import varsa .* mantıklı
+            - Insufficient test: if, else if else kadar unit testiniz olması lazım
+            - Test should be fast
          */
     }
 }
